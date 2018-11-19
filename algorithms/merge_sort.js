7b287d1df707c17e4cfd6ea5f17908536d6be862
NikSS <nik.safe@gmail.com>
2018-11-19 11:55:16 -0800

    add merge function

--- algorithms/merge_sort.js
+++ algorithms/merge_sort.js
@@ -1,1 +1,21 @@
 // implementation of mergeSort
+
+function merge(left, right) {
+  const result = [];
+  let indexLeft = 0;
+  let indexRight = 0;
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft]);
+      indexLeft++;
+    } else {
+      result.push(right[indexRight]);
+      indexRight++;
+    }
+  }
+
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+}
+merge([1, 3], [4, 5]);
+
